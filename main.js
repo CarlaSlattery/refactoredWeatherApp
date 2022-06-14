949e67a200ce262664b1858d720934be94d13f0d
Carla Slattery <106931504+CarlaSlattery@users.noreply.github.com>
2022-06-14 07:26:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+//=================Date & Time================================//
+
+let now = new Date();
+
+let todaysDate = document.querySelector("#dateTime");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+todaysDate.innerHTML = `${day}, ${date} ${month} ${year} at ${hours}:${minutes}`;
