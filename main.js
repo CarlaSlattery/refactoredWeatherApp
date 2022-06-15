67f26116ea99f836ec45ef012df66105c181db4e
Carla Slattery <106931504+CarlaSlattery@users.noreply.github.com>
2022-06-15 10:20:35 +0100

    Added weather icon functionality

--- main.js
+++ main.js
@@ -43,3 +43,32 @@ let months = [
 let month = months[now.getMonth()];
 
 todaysDate.innerHTML = `${day}, ${date} ${month} ${year} at ${hours}:${minutes}`;
+
+// =================== WEATHER SEARCH ===========================================//
+
+function displayCurrentTemp(response) {
+  console.log(response.data);
+  let city = document.querySelector("#placeName");
+  let currentTemp = document.querySelector("#currentTemp");
+  let weatherDescription = document.querySelector("#weatherDescription");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#windSpeed");
+  let iconCurrent = document.querySelector("#currentWeatherIcon");
+  city.innerHTML = response.data.name;
+  currentTemp.innerHTML = Math.round(response.data.main.temp);
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  iconCurrent.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconCurrent.setAttribute("alt", response.data.weather[0].description);
+}
+
+let city = "Stoke";
+let apiKey = "51a8ffee2e435fe855f1dad6b24620d1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayCurrentTemp);
